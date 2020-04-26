d480d18fa52b7170f114e5a9198fad7f48ec31a3
Greg Jett <greg.jett25@gmail.com>
2020-04-26 16:42:49 -0500

    js intro

---
+++ js/js-intro.js
@@ -0,0 +1,12 @@
+
+"use strict";
+
+// alert('You won $1,000,000.00!!!!!!');
+//
+// var willContinue = confirm("Will you continue?");
+//
+// console.log(willContinue);
+
+var favNum = Number(prompt("Please enter your favorite number:"));
+
+console.log(favNum + favNum);
