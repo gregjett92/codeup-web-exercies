42fb549658df352fa50ffa2347347b8e4835efca
Greg Jett <greg.jett25@gmail.com>
2020-05-04 00:39:20 -0500

    add objects html and js

--- js/planets-string.js
+++ js/planets-string.js
@@ -11,17 +11,35 @@
      * console.log planetsArray to check your work
      */
 
+    var planetArray = planetsString.split('|');
+    console.log(planetsString)
+    console.log(planetArray);
+
+
+
     // console.log(planetsArray);
 
     /**
      * TODO:
      * Create a string with <br> tags between each planet. console.log() your
      * results. Why might this be useful?
+     */
+    var newPlanet = planetArray.join('<br>');
+
+    console.log(newPlanet);
+     /*
      *
      * BONUS:
      * Create another string that would display your planets in an undordered
      * list. You will need an opening AND closing <ul> tags around the entire
      * string, and <li> tags around each planet.
-     */
+
+      */
+    function returnList(arr) {}
+    var planetsList = "<ul>";
+    planetArray.forEach(function(planet){
+    });
+        planetsList += "<ul>";
+
 
 })();
