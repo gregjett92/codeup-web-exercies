016a48151fde3a54771dd4384c1e48085efb1af6
Greg Jett <greg.jett25@gmail.com>
2020-05-14 16:25:32 -0500

    jquery faq

--- js/essential-methods-lec.js
+++ js/essential-methods-lec.js
@@ -30,18 +30,48 @@ $(document).ready(function() {
 
     // TODO TOGETHER:  Using an event listener and css method, increase the font-size of 'Sorting Hat' when clicked.
     //    How can font size be changed using .html()?
+    $('#main-heading').click(function(){
+        // $(this).css('font-size', "4em");
+        $(this).html('<h4>My sorting Har</h4>')
+    });
+
+    $('ul').click(function() {
+      $(this).html('<li>Test</li>')
+    })
 
     // TODO TOGETHER: store the inner text of #main-heading in a variable
+    var mainHeading = $('#main-heading').text();
+    alert(mainHeading)
 
     // TODO: Using a css method, change the banner background-color to black
     //  Hint: make sure you use the right selector
 
-    // TODO TOGETHER: When the 'Houses' button is clicked, add the class 'house' to anything with a class of 'house-name'
+    $('.banner').css('background-color', '#000');
 
+    // TODO TOGETHER: When the 'Houses' button is clicked, add the class 'house' to anything with a class of 'house-name'
+    // Click on Button with this id
+       $('#highlight-houses').click(function () {
+           //target all elements with class of .house-name
+           //add class "house to its html"
+           // $('.house-name').addClass('house')
+           // $('.house-name').removeClass('house');
+           $('#highlight-houses').toggle();
+
+       }
     // TODO: Comment out the code above. Add the class of 'house' to all the 'house-names'
     //  Write the event listener to remove the class instead
 
     // TODO TOGETHER: Refactor your addClass and removeClass methods to toggle the 'house' class instead
+    $('#lock-g').click(function(){
+        $('#gryffindor-house').toggleClass('gryffindor');
+        if($('gryffindor-house').hasClass('gryffindor')){
+            //
+            $(this).text('unlock');
+        }else {
+            $(this).
+        }
+
+    })
 
     /**********************************************
      * 			       Traversing
